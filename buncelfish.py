a0de1f89c178d32ee3485e616a97d90f16e82060
Alex Wootton <agwootton@gmail.com>
2017-11-13 09:48:13 +0000

    Translates from Buncish to English only
    contains code for expanded translation but needs further work

---
+++ buncelfish.py
@@ -0,0 +1,132 @@
+import os
+import random
+import sys
+from translation import Dictionary
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def print_logo():
+    print(u'''
+
+               Welcome to
+
+    ____                             __
+   / __ ) __  __ ____   _____ ___   / /
+  / __  |/ / / // __ \ / ___// _ \ / /
+ / /_/ // /_/ // / / // /__ /  __// /
+/_____/ \__,_//_/ /_/ \___/ \___//_/
+         ______ _        __
+        / ____/(_)_____ / /_
+       / /_   / // ___// __ \ ™
+      / __/  / /(__  )/ / / /
+     /_/    /_//____//_/ /_/
+
+
+  Numero Uno in unversal translators
+     *Currently only accepting*
+     *single word translations*
+''')
+
+def debunce():
+    dict_file = "default_dict.csv"
+    dictionary = Dictionary(dict_file)
+    languages = dictionary.languages
+    word = None
+    origin = None
+    destination = None
+    translation = None
+    while True:
+        clear_screen()
+        print_logo()
+        if not word:
+            word = input("Buncism: ")
+            if word == "":
+                print("You need to provide a buncism to debunce")
+                input("\npress ENTER to continue\n")
+                word = None
+                continue
+        clear_screen()
+        print_logo()
+        try:
+            translation = dictionary.translate(word, 'Buncish', 'English')
+            if isinstance(translation, str):
+                print("'{}' is '{}'".format(word, translation))
+            elif isinstance(translation, list):
+                print("A direct match couldn't be found for '{}'".format(word))
+                print("Here are some near matches:")
+                print("\n".join(translation))
+            if input("\nDebunce again? [Y/n]: ").lower() == "n":
+                print("Come back soon!")
+                sys.exit()
+        except ValueError as err:
+            print(err)
+            input("\npress ENTER to continue\n")
+        break
+
+def translate():
+    done = False
+    dict_file = "default_dict.csv"
+    dictionary = Dictionary(dict_file)
+    languages = dictionary.languages
+    word = None
+    origin = None
+    destination = None
+    translation = None
+    while not done:
+        clear_screen()
+        print_logo()
+        if not word:
+            word = input("Word to translate: ")
+            if word == "":
+                print("You need to provide a word to translate")
+                input("\npress ENTER to continue\n")
+                word = None
+                continue
+        clear_screen()
+        print_logo()
+        if not origin:
+            origin = input("Translate from: ").capitalize()
+            if origin not in languages:
+                print("Looks like that's not a supported language.")
+                print("You can choose from the following:")
+                print("{}".format("\n".join(languages)))
+                input("\npress ENTER to continue\n")
+                origin = None
+                continue
+        clear_screen()
+        print_logo()
+        if not destination:
+            destination = input("Translate to: ").capitalize()
+            if destination not in languages:
+                print("That's not a supported language.")
+                print("You can choose from the following:")
+                print("{}".format("\n".join(languages)))
+                input("\npress ENTER to continue\n")
+                destination = None
+                continue
+        clear_screen()
+        print_logo()
+        try:
+            translation = dictionary.translate(word, origin, destination)
+            print("The {} word '{}' is '{}' in {}".format(origin, word,
+                                                          translation,
+                                                          destination))
+            if input("\nTranslate another word? [Y/n]: ").lower() == "n":
+                running = False
+                done = True
+        except ValueError as err:
+            print(err)
+            input("\npress ENTER to continue\n")
+        break
+
+def main():
+    running = True
+    clear_screen()
+    print_logo()
+    input("        press ENTER to start")
+    while running:
+        #translate()
+        debunce()
+
+main()
---
+++ translation.py
@@ -0,0 +1,66 @@
+import csv
+
+class Dictionary(list):
+    """A list of dictionaries containing words/phrases in several languages.
+
+    Each dictionary contains one word or phrase in several languages as read
+    from a provided .csv file.
+    The key is the language and the value is the word in that language.
+    """
+
+    def __init__(self, file='default_dict.csv', *args, **kwargs):
+        if not isinstance(file, str):
+            raise ValueError("File must be a string")
+        super().__init__()
+        self.file = open(file, "r")
+        if file[-4:] == ".csv":
+            reader = csv.DictReader(self.file)
+            for line in reader:
+                self.append(line)
+        else:
+            raise ValueError("Must be a .csv file")
+            # for line in self.file:
+                # key, value = line.split(",")
+                # self[key] = value
+        self.file.close()
+
+    @property
+    def languages(self):
+        lang_list = []
+        for key in self[0]:
+            lang_list.append(key)
+        return lang_list
+
+    def translate(self, word=None, origin=None, destination=None):
+        possible_matches = []
+        if not word:
+            raise ValueError("You must provide a word to translate")
+        if not origin:
+            # search all dictionaries for word and try to translate to destination
+            for dictionary in self:
+                for key in dictionary:
+                    if word.lower() == dictionary[key].lower():
+                        return dictionary[destination]
+                    elif word.lower() in dictionary[key].lower():
+                        possible_matches.append(dictionary[key])
+            raise ValueError("'{}' could not be found in any dictionary".format(word))
+            return None
+        if not destination:
+            # return all available translations for word as dictionary
+            for dictionary in self:
+                if word.lower() in dictionary[origin].lower():
+                    translations = {}
+                    for key in dictionary:
+                        translations[key] = dictionary[key]
+                    return translations
+        for dictionary in self:
+            if word.lower() == dictionary[origin].lower():
+                return dictionary[destination]
+            elif word.lower() in dictionary[origin].lower():
+                #import pdb; pdb.set_trace()
+                possible_matches.append("'{}' meaning '{}'".format(dictionary[origin], dictionary[destination]))
+        if possible_matches:
+            return possible_matches
+        else:
+            raise ValueError("'{}' is not in the {} dictionary".format(word, origin.capitalize()))
+            return None
